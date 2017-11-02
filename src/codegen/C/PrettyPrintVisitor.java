96537658a5377abeb0b61d55ee0712a3fa87da2e
Waydrow <waydrow@163.com>
2017-11-02 16:42:44 +0800

    finish PrettyPrinter of C

--- src/codegen/C/PrettyPrintVisitor.java
+++ src/codegen/C/PrettyPrintVisitor.java
@@ -36,395 +36,406 @@ import codegen.C.Ast.Vtable;
 import codegen.C.Ast.Vtable.VtableSingle;
 import control.Control;
 
-public class PrettyPrintVisitor implements Visitor
-{
-  private int indentLevel;
-  private java.io.BufferedWriter writer;
+public class PrettyPrintVisitor implements Visitor {
+    private int indentLevel;
+    private java.io.BufferedWriter writer;
 
-  public PrettyPrintVisitor()
-  {
-    this.indentLevel = 2;
-  }
-
-  private void indent()
-  {
-    this.indentLevel += 2;
-  }
-
-  private void unIndent()
-  {
-    this.indentLevel -= 2;
-  }
-
-  private void printSpaces()
-  {
-    int i = this.indentLevel;
-    while (i-- != 0)
-      this.say(" ");
-  }
-
-  private void sayln(String s)
-  {
-    say(s);
-    try {
-      this.writer.write("\n");
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
+    public PrettyPrintVisitor() {
+        this.indentLevel = 2;
     }
-  }
 
-  private void say(String s)
-  {
-    try {
-      this.writer.write(s);
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
+    private void indent() {
+        this.indentLevel += 2;
     }
-  }
 
-  // /////////////////////////////////////////////////////
-  // expressions
-  @Override
-  public void visit(Add e)
-  {
-  }
-
-  @Override
-  public void visit(And e)
-  {
-  }
-
-  @Override
-  public void visit(ArraySelect e)
-  {
-  }
-
-  @Override
-  public void visit(Call e)
-  {
-    this.say("(" + e.assign + "=");
-    e.exp.accept(this);
-    this.say(", ");
-    this.say(e.assign + "->vptr->" + e.id + "(" + e.assign);
-    int size = e.args.size();
-    if (size == 0) {
-      this.say("))");
-      return;
+    private void unIndent() {
+        this.indentLevel -= 2;
     }
-    for (Exp.T x : e.args) {
-      this.say(", ");
-      x.accept(this);
+
+    private void printSpaces() {
+        int i = this.indentLevel;
+        while (i-- != 0)
+            this.say(" ");
     }
-    this.say("))");
-    return;
-  }
 
-  @Override
-  public void visit(Id e)
-  {
-    this.say(e.id);
-  }
+    private void sayln(String s) {
+        say(s);
+        try {
+            this.writer.write("\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
-  @Override
-  public void visit(Length e)
-  {
-  }
+    private void say(String s) {
+        try {
+            this.writer.write(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
-  @Override
-  public void visit(Lt e)
-  {
-    e.left.accept(this);
-    this.say(" < ");
-    e.right.accept(this);
-    return;
-  }
+    // /////////////////////////////////////////////////////
+    // expressions
+    @Override
+    public void visit(Add e) {
+        e.left.accept(this);
+        this.say(" + ");
+        e.right.accept(this);
+        return;
+    }
 
-  @Override
-  public void visit(NewIntArray e)
-  {
-  }
+    @Override
+    public void visit(And e) {
+        e.left.accept(this);
+        this.say(" && ");
+        e.right.accept(this);
+        return;
+    }
 
-  @Override
-  public void visit(NewObject e)
-  {
-    this.say("((struct " + e.id + "*)(Tiger_new (&" + e.id
-        + "_vtable_, sizeof(struct " + e.id + "))))");
-    return;
-  }
+    @Override
+    public void visit(ArraySelect e) {
+        e.array.accept(this);
+        this.say("[");
+        e.index.accept(this);
+        this.say("]");
+        return;
+    }
 
-  @Override
-  public void visit(Not e)
-  {
-  }
-
-  @Override
-  public void visit(Num e)
-  {
-    this.say(Integer.toString(e.num));
-    return;
-  }
-
-  @Override
-  public void visit(Sub e)
-  {
-    e.left.accept(this);
-    this.say(" - ");
-    e.right.accept(this);
-    return;
-  }
-
-  @Override
-  public void visit(This e)
-  {
-    this.say("this");
-  }
-
-  @Override
-  public void visit(Times e)
-  {
-    e.left.accept(this);
-    this.say(" * ");
-    e.right.accept(this);
-    return;
-  }
-
-  // statements
-  @Override
-  public void visit(Assign s)
-  {
-    this.printSpaces();
-    this.say(s.id + " = ");
-    s.exp.accept(this);
-    this.say(";");
-    return;
-  }
-
-  @Override
-  public void visit(AssignArray s)
-  {
-  }
-
-  @Override
-  public void visit(Block s)
-  {
-  }
-
-  @Override
-  public void visit(If s)
-  {
-    this.printSpaces();
-    this.say("if (");
-    s.condition.accept(this);
-    this.sayln(")");
-    this.indent();
-    s.thenn.accept(this);
-    this.unIndent();
-    this.sayln("");
-    this.printSpaces();
-    this.sayln("else");
-    this.indent();
-    s.elsee.accept(this);
-    this.sayln("");
-    this.unIndent();
-    return;
-  }
-
-  @Override
-  public void visit(Print s)
-  {
-    this.printSpaces();
-    this.say("System_out_println (");
-    s.exp.accept(this);
-    this.sayln(");");
-    return;
-  }
-
-  @Override
-  public void visit(While s)
-  {
-  }
-
-  // type
-  @Override
-  public void visit(ClassType t)
-  {
-    this.say("struct " + t.id + " *");
-  }
-
-  @Override
-  public void visit(Int t)
-  {
-    this.say("int");
-  }
-
-  @Override
-  public void visit(IntArray t)
-  {
-  }
-
-  // dec
-  @Override
-  public void visit(DecSingle d)
-  {
-  }
-
-  // method
-  @Override
-  public void visit(MethodSingle m)
-  {
-    m.retType.accept(this);
-    this.say(" " + m.classId + "_" + m.id + "(");
-    int size = m.formals.size();
-    for (Dec.T d : m.formals) {
-      DecSingle dec = (DecSingle) d;
-      size--;
-      dec.type.accept(this);
-      this.say(" " + dec.id);
-      if (size > 0)
+    @Override
+    public void visit(Call e) {
+        this.say("(" + e.assign + "=");
+        e.exp.accept(this);
         this.say(", ");
-    }
-    this.sayln(")");
-    this.sayln("{");
-
-    for (Dec.T d : m.locals) {
-      DecSingle dec = (DecSingle) d;
-      this.say("  ");
-      dec.type.accept(this);
-      this.say(" " + dec.id + ";\n");
-    }
-    this.sayln("");
-    for (Stm.T s : m.stms)
-      s.accept(this);
-    this.say("  return ");
-    m.retExp.accept(this);
-    this.sayln(";");
-    this.sayln("}");
-    return;
-  }
-
-  @Override
-  public void visit(MainMethodSingle m)
-  {
-    this.sayln("int Tiger_main ()");
-    this.sayln("{");
-    for (Dec.T dec : m.locals) {
-      this.say("  ");
-      DecSingle d = (DecSingle) dec;
-      d.type.accept(this);
-      this.say(" ");
-      this.sayln(d.id + ";");
-    }
-    m.stm.accept(this);
-    this.sayln("}\n");
-    return;
-  }
-
-  // vtables
-  @Override
-  public void visit(VtableSingle v)
-  {
-    this.sayln("struct " + v.id + "_vtable");
-    this.sayln("{");
-    for (codegen.C.Ftuple t : v.ms) {
-      this.say("  ");
-      t.ret.accept(this);
-      this.sayln(" (*" + t.id + ")();");
-    }
-    this.sayln("};\n");
-    return;
-  }
-
-  private void outputVtable(VtableSingle v)
-  {
-    this.sayln("struct " + v.id + "_vtable " + v.id + "_vtable_ = ");
-    this.sayln("{");
-    for (codegen.C.Ftuple t : v.ms) {
-      this.say("  ");
-      this.sayln(t.classs + "_" + t.id + ",");
-    }
-    this.sayln("};\n");
-    return;
-  }
-
-  // class
-  @Override
-  public void visit(ClassSingle c)
-  {
-    this.sayln("struct " + c.id);
-    this.sayln("{");
-    this.sayln("  struct " + c.id + "_vtable *vptr;");
-    for (codegen.C.Tuple t : c.decs) {
-      this.say("  ");
-      t.type.accept(this);
-      this.say(" ");
-      this.sayln(t.id + ";");
-    }
-    this.sayln("};");
-    return;
-  }
-
-  // program
-  @Override
-  public void visit(ProgramSingle p)
-  {
-    // we'd like to output to a file, rather than the "stdout".
-    try {
-      String outputName = null;
-      if (Control.ConCodeGen.outputName != null)
-        outputName = Control.ConCodeGen.outputName;
-      else if (Control.ConCodeGen.fileName != null)
-        outputName = Control.ConCodeGen.fileName + ".c";
-      else
-        outputName = "a.c";
-
-      this.writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(
-          new java.io.FileOutputStream(outputName)));
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
+        this.say(e.assign + "->vptr->" + e.id + "(" + e.assign);
+        int size = e.args.size();
+        if (size == 0) {
+            this.say("))");
+            return;
+        }
+        for (Exp.T x : e.args) {
+            this.say(", ");
+            x.accept(this);
+        }
+        this.say("))");
+        return;
     }
 
-    this.sayln("// This is automatically generated by the Tiger compiler.");
-    this.sayln("// Do NOT modify!\n");
-
-    this.sayln("// structures");
-    for (codegen.C.Ast.Class.T c : p.classes) {
-      c.accept(this);
+    @Override
+    public void visit(Id e) {
+        this.say(e.id);
     }
 
-    this.sayln("// vtables structures");
-    for (Vtable.T v : p.vtables) {
-      v.accept(this);
-    }
-    this.sayln("");
-
-    this.sayln("// methods");
-    for (Method.T m : p.methods) {
-      m.accept(this);
-    }
-    this.sayln("");
-
-    this.sayln("// vtables");
-    for (Vtable.T v : p.vtables) {
-      outputVtable((VtableSingle) v);
-    }
-    this.sayln("");
-
-    this.sayln("// main method");
-    p.mainMethod.accept(this);
-    this.sayln("");
-
-    this.say("\n\n");
-
-    try {
-      this.writer.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
+    @Override
+    public void visit(Length e) {
+        this.say("sizeof(");
+        e.array.accept(this);
+        this.say(")");
     }
 
-  }
+    @Override
+    public void visit(Lt e) {
+        e.left.accept(this);
+        this.say(" < ");
+        e.right.accept(this);
+        return;
+    }
+
+    @Override
+    public void visit(NewIntArray e) {
+        this.say("((int*)(Tiger_new_array(");
+        e.exp.accept(this);
+        this.say(")))");
+        return;
+    }
+
+    @Override
+    public void visit(NewObject e) {
+        this.say("((struct " + e.id + "*)(Tiger_new (&" + e.id
+                + "_vtable_, sizeof(struct " + e.id + "))))");
+        return;
+    }
+
+    @Override
+    public void visit(Not e) {
+        this.say("!");
+        e.exp.accept(this);
+        return;
+    }
+
+    @Override
+    public void visit(Num e) {
+        this.say(Integer.toString(e.num));
+        return;
+    }
+
+    @Override
+    public void visit(Sub e) {
+        e.left.accept(this);
+        this.say(" - ");
+        e.right.accept(this);
+        return;
+    }
+
+    @Override
+    public void visit(This e) {
+        this.say("this");
+    }
+
+    @Override
+    public void visit(Times e) {
+        e.left.accept(this);
+        this.say(" * ");
+        e.right.accept(this);
+        return;
+    }
+
+    // statements
+    @Override
+    public void visit(Assign s) {
+        this.printSpaces();
+        this.say(s.id + " = ");
+        s.exp.accept(this);
+        this.say(";");
+        return;
+    }
+
+    @Override
+    public void visit(AssignArray s) {
+        this.printSpaces();
+        this.say(s.id + "[");
+        s.index.accept(this);
+        this.say("] = ");
+        s.exp.accept(this);
+        this.sayln(";");
+        return;
+    }
+
+    @Override
+    public void visit(Block s) {
+        for (Stm.T stm : s.stms) {
+            stm.accept(this);
+        }
+        return;
+    }
+
+    @Override
+    public void visit(If s) {
+        this.printSpaces();
+        this.say("if (");
+        s.condition.accept(this);
+        this.sayln(")");
+        this.indent();
+        s.thenn.accept(this);
+        this.unIndent();
+        this.sayln("");
+        this.printSpaces();
+        this.sayln("else");
+        this.indent();
+        s.elsee.accept(this);
+        this.sayln("");
+        this.unIndent();
+        return;
+    }
+
+    @Override
+    public void visit(Print s) {
+        this.printSpaces();
+        this.say("System_out_println (");
+        s.exp.accept(this);
+        this.sayln(");");
+        return;
+    }
+
+    @Override
+    public void visit(While s) {
+        this.printSpaces();
+        this.say("while (");
+        s.condition.accept(this);
+        this.sayln(") {");
+        this.indent();
+        s.body.accept(this);
+        this.unIndent();
+        this.printSpaces();
+        this.sayln("}");
+        return;
+    }
+
+    // type
+    @Override
+    public void visit(ClassType t) {
+        this.say("struct " + t.id + " *");
+    }
+
+    @Override
+    public void visit(Int t) {
+        this.say("int");
+    }
+
+    @Override
+    public void visit(IntArray t) {
+        this.say("int*");
+    }
+
+    // dec
+    @Override
+    public void visit(DecSingle d) {
+        this.printSpaces();
+        d.type.accept(this);
+        this.sayln(" " + d.id + ";");
+    }
+
+    // method
+    @Override
+    public void visit(MethodSingle m) {
+        m.retType.accept(this);
+        this.say(" " + m.classId + "_" + m.id + "(");
+        int size = m.formals.size();
+        for (Dec.T d : m.formals) {
+            DecSingle dec = (DecSingle) d;
+            size--;
+            dec.type.accept(this);
+            this.say(" " + dec.id);
+            if (size > 0)
+                this.say(", ");
+        }
+        this.sayln(")");
+        this.sayln("{");
+
+        for (Dec.T d : m.locals) {
+            DecSingle dec = (DecSingle) d;
+            this.say("  ");
+            dec.type.accept(this);
+            this.say(" " + dec.id + ";\n");
+        }
+        this.sayln("");
+        for (Stm.T s : m.stms)
+            s.accept(this);
+        this.say("  return ");
+        m.retExp.accept(this);
+        this.sayln(";");
+        this.sayln("}");
+        return;
+    }
+
+    @Override
+    public void visit(MainMethodSingle m) {
+        this.sayln("int Tiger_main ()");
+        this.sayln("{");
+        for (Dec.T dec : m.locals) {
+            this.say("  ");
+            DecSingle d = (DecSingle) dec;
+            d.type.accept(this);
+            this.say(" ");
+            this.sayln(d.id + ";");
+        }
+        m.stm.accept(this);
+        this.sayln("}\n");
+        return;
+    }
+
+    // vtables
+    @Override
+    public void visit(VtableSingle v) {
+        this.sayln("struct " + v.id + "_vtable");
+        this.sayln("{");
+        for (codegen.C.Ftuple t : v.ms) {
+            this.say("  ");
+            t.ret.accept(this);
+            this.sayln(" (*" + t.id + ")();");
+        }
+        this.sayln("};\n");
+        return;
+    }
+
+    private void outputVtable(VtableSingle v) {
+        this.sayln("struct " + v.id + "_vtable " + v.id + "_vtable_ = ");
+        this.sayln("{");
+        for (codegen.C.Ftuple t : v.ms) {
+            this.say("  ");
+            this.sayln(t.classs + "_" + t.id + ",");
+        }
+        this.sayln("};\n");
+        return;
+    }
+
+    // class
+    @Override
+    public void visit(ClassSingle c) {
+        this.sayln("struct " + c.id);
+        this.sayln("{");
+        this.sayln("  struct " + c.id + "_vtable *vptr;");
+        for (codegen.C.Tuple t : c.decs) {
+            this.say("  ");
+            t.type.accept(this);
+            this.say(" ");
+            this.sayln(t.id + ";");
+        }
+        this.sayln("};");
+        return;
+    }
+
+    // program
+    @Override
+    public void visit(ProgramSingle p) {
+        // we'd like to output to a file, rather than the "stdout".
+        try {
+            String outputName = null;
+            if (Control.ConCodeGen.outputName != null)
+                outputName = Control.ConCodeGen.outputName;
+            else if (Control.ConCodeGen.fileName != null)
+                outputName = Control.ConCodeGen.fileName + ".c";
+            else
+                outputName = "a.c";
+
+            this.writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(
+                    new java.io.FileOutputStream(outputName)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        this.sayln("// This is automatically generated by the Tiger compiler.");
+        this.sayln("// Do NOT modify!\n");
+
+        this.sayln("// structures");
+        for (codegen.C.Ast.Class.T c : p.classes) {
+            c.accept(this);
+        }
+
+        this.sayln("// vtables structures");
+        for (Vtable.T v : p.vtables) {
+            v.accept(this);
+        }
+        this.sayln("");
+
+        this.sayln("// methods");
+        for (Method.T m : p.methods) {
+            m.accept(this);
+        }
+        this.sayln("");
+
+        this.sayln("// vtables");
+        for (Vtable.T v : p.vtables) {
+            outputVtable((VtableSingle) v);
+        }
+        this.sayln("");
+
+        this.sayln("// main method");
+        p.mainMethod.accept(this);
+        this.sayln("");
+
+        this.say("\n\n");
+
+        try {
+            this.writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
 
 }
